da972f088a88b21cf2f2a6fdc738eae46a40b37f
Sanjay Timilsena <chhsanjay123@gmail.com>
2022-11-13 08:20:38 +0100

    Added openai library and configure api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 function App() {
   return (
     <div className="App">
